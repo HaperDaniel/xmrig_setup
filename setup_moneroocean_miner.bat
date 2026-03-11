0ae4bec7c5850c0f38714636a9ca4dfc7813e1de
HaperDaniel <264435133+HaperDaniel@users.noreply.github.com>
2026-03-10 19:53:41 -0700

    Update setup_moneroocean_miner.bat

--- setup_moneroocean_miner.bat
+++ setup_moneroocean_miner.bat
@@ -125,7 +125,7 @@ if %ADMIN% == 0 (
 echo.
 echo JFYI: This host has %NUMBER_OF_PROCESSORS% CPU threads, so projected Monero hashrate is around %EXP_MONERO_HASHRATE% KH/s.
 echo.
-
+pause
 rem start doing stuff: preparing miner
 
 echo [*] Removing previous moneroocean miner (if any)
